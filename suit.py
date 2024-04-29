6769901e5c3af04257b936e730912c75b4eade14
TaKa <akmalkaflianan33@gmail.com>
2024-04-29 07:58:35 +0700

    menambahkan keterangan pilihan komputer dan pemain danmerapihkan serta menghapus histori terminal saat program dimulai

--- suit.py
+++ suit.py
@@ -1,24 +1,42 @@
 import random 
+import os
+
+os.system('cls')
 print("\n                     Suit                      ")
 print("                  by: Taka.py                  ")
 print("===============================================")
 
+trans = {
+    1 : 'Gunting',
+    2 : 'Batu',
+    3 : 'Kertas',
+    'g' : 'Gunting',
+    'b' : 'Batu',
+    'k' : 'Kertas',
+}
+
 while True:
     komputer = random.randint(1,3)
-    manusia = input('Pilih antara gunting/batu/kertas \n[g/b/k]')
+    manusia = input('Pilih antara gunting/batu/kertas \n[g/b/k] \n>>> ')
     
     if manusia in ['g','b','k']:
         break
     else:
-        print('Input salah!')
+        print('\nInput salah!')
 
 if komputer == 1 and manusia == 'b' or  komputer == 2 and manusia == 'k' or komputer == 3 and manusia == 'g':
-   print('kamu menang!')
+    print(f'\nKomputer: {trans[komputer]}')
+    print(f'Kamu: {trans[manusia]}')
+    print('kamu menang!')
     
 elif komputer == 1 and manusia == 'k' or  komputer == 2 and manusia == 'g' or komputer == 3 and manusia == 'b':
-   print('kamu kalah!')
+    print(f'\nKomputer: {trans[komputer]}')
+    print(f'Kamu: {trans[manusia]}')
+    print('kamu kalah!')
    
 else:
+    print(f'\nKomputer: {trans[komputer]}')
+    print(f'Kamu: {trans[manusia]}')
     print('Seri!')
     
-print('program selesai...')
+print('\nprogram selesai...')
