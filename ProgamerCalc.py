bf34e2984f9e997b62845e60851086398bd88428
TaKa <akmalkaflianan33@gmail.com>
2024-09-01 07:03:35 +0700

    membuat kalkulator dari desimal ke biner

---
+++ ProgamerCalc.py
@@ -0,0 +1,13 @@
+# desimal ke biner
+
+user = float(input('masukan angka :'))
+bit = ''
+while(user):
+    if user != 1:
+        bit = bit + str(int(user % 2))
+        user = user // 2
+    else:
+        bit = bit + '1'
+        break
+print(bit[::-1])
+print(user)
