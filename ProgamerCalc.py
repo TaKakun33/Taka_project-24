fbbb59662ae48f92f8b2ceb510c5254c7db27c56
TaKa <akmalkaflianan33@gmail.com>
2024-09-01 11:06:38 +0700

    menambahkan fungsi agar bisa konversi desimal ke biner berseta yang dibelakang koma

--- ProgamerCalc.py
+++ ProgamerCalc.py
@@ -1,13 +1,39 @@
 # desimal ke biner
-user = float(input('masukan angka :'))
+user = input('masukan angka :')
 bit = ''
+underbit = '.'
 
-while(user):
-    if user != 1:
-        bit = bit + str(int(user % 2))
-        user = user // 2
+Split_user = user.split('.')
+if len(Split_user)== 2:     
+    user_1 = int(Split_user[0])
+    user_2 = '0.' + Split_user[1]
+    user_2 = float(user_2)
+else:
+    user_1 = int(Split_user[0])
+    user_2 = 0
+    underbit = ''
+    
+while(user_1):
+    if user_1 != 1:
+        bit = bit + str(user_1 % 2)
+        user_1 = user_1 // 2
     else:
         bit = bit + '1'
         break
-print(bit[::-1])
-print(user)
+    
+while(user_2):
+    user_2 = user_2 * 2
+    if user_2 >= 1:
+         user_2 = user_2 - 1
+         underbit = underbit + '1'
+    else:
+        underbit = underbit + '0'
+
+bit = bit[::-1]
+print(bit)
+
+underbit = '%.18s' % underbit
+print(underbit)
+
+biner = bit + underbit
+print(biner)
